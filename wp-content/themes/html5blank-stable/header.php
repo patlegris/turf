f2f30dbadf0e1e07e12de646a3a38628d33ea679
Patlegris <lecointre.patrick@laposte.net>
2016-01-24 21:00:53 +0100

    first commit

--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -1,50 +1,63 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php wp_title(''); ?><?php if (wp_title('', false)) {
+            echo ' :';
+        } ?><?php bloginfo('name'); ?></title>
 
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+    <link href="//www.google-analytics.com" rel="dns-prefetch">
+    <link href="<?php echo get_template_directory_uri(); ?>img/icons/favicon.ico" rel="shortcut icon">
+    <link href="<?php echo get_template_directory_uri(); ?>img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+    <!--Google Fonts CDN   -->
+    <link href='https://fonts.googleapis.com/css?family=Tangerine|Droid+Sans:400,700|Roboto+Slab:400,700'
+          rel='stylesheet' type='text/css'>
+    <style>
+        body {
+            font-family: 'Tangerine', serif;
+            font-size: 16px;
+        }
+    </style>
+    <!--End Google Fonts CDN   -->
 
-		<?php wp_head(); ?>
-		<script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+
+    <?php wp_head(); ?>
+    <script>
         // conditionizr.com
         // configure environment tests
         conditionizr.config({
             assets: '<?php echo get_template_directory_uri(); ?>',
             tests: {}
         });
-        </script>
+    </script>
 
-	</head>
-	<body <?php body_class(); ?>>
+</head>
+<body <?php body_class(); ?>>
 
-		<!-- wrapper -->
-		<div class="wrapper">
+<!-- wrapper -->
+<div class="wrapper">
 
-			<!-- header -->
-			<header class="header clear" role="banner">
+    <!-- header -->
+    <header class="header clear" role="banner">
 
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
-					<!-- /logo -->
+        <!-- logo -->
+        <div class="logo">
+            <a href="<?php echo home_url(); ?>">
+                <!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+            </a>
+        </div>
+        <!-- /logo -->
 
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
+        <!-- nav -->
+        <nav class="nav" role="navigation">
+            <?php html5blank_nav(); ?>
+        </nav>
+        <!-- /nav -->
 
-			</header>
-			<!-- /header -->
+    </header>
+    <!-- /header -->
